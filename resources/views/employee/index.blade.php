b5fadf75610a16f52b5144705edabe10880a62fd
Jojiya <jojiya12@gmail.com>
2023-09-21 00:32:15 +0530

    hr index

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,25 +1,13 @@
 <x-app-layout>
-    <div class="w-11/12 mx-auto mt-16 flex items-center justify-between">
+    <x-slot name="header">
+        {{ __('Employees') }}
+    </x-slot>
+    <div class="w-full flex items-center justify-end -mt-20">
         <div>
-            <h2 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Employees') }}
-            </h2>
-        </div>
-        <div>
-            <!-- Open Modal Button -->
             <a href="{{ route('employee.create') }}">
                 <x-primary-button>
-                    Add Employee
+                    Add Employees
                 </x-primary-button>
-                @if (session('status') === 'added sucessfully')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Added sucessfully') }}</p>
-            @endif
             </a>
         </div>
     </div>
